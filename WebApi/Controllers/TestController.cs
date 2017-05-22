26003bfb621d2b6a5d99f80105b42c9b55a57b7d
Shawn South <shawn.south@k2.com>
2017-05-22 12:08:14 -0700

    CORS enabled
    Per tutorial, via attribute, etc.

--- WebApi/Controllers/TestController.cs
+++ WebApi/Controllers/TestController.cs
@@ -2,9 +2,12 @@
 {
     using System.Net.Http;
     using System.Web.Http;
+    using System.Web.Http.Cors;
 
+    //[EnableCors(origins: "http://localhost:3017", headers:"*", methods:"*")]
     public class TestController : ApiController
     {
+        [EnableCors(origins: "http://localhost:3017", headers: "*", methods: "GET")]
         public HttpResponseMessage Get()
         {
             return new HttpResponseMessage()
